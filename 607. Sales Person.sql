b33bc9d6925d2138a23bbf4ad2141f6c3f21b8e6
Praveen Chandrasekaran <81367485+Praveen2795@users.noreply.github.com>
2023-11-13 19:14:15 -0500

    done with prob 2 in sql4

--- 607. Sales Person.sql
+++ 607. Sales Person.sql
@@ -1,11 +1,12 @@
-with cte1 as (select requester_id
-from RequestAccepted
-union all
-select accepter_id
-from RequestAccepted)
-
-select requester_id as id, count(requester_id) as num
+with cte1 as (select s.sales_id, s.name, o.com_id
+from Orders as o
+right join SalesPerson as s
+on o.sales_id = s.sales_id),
+cte2 as (select cte1.name as person, c.name as com_name
 from cte1
-group by requester_id
-order by num desc
-limit 1
+left join Company as c
+on cte1.com_id = c.com_id)
+
+SELECT distinct person as name
+FROM cte2
+WHERE person not in (select person from cte2 where com_name = 'RED')
